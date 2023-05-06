2184dc2a965d9a994f2311e352897d7f29f07788
Chiao75543 <sin75543@gmail.com>
2023-05-06 16:28:20 +0800

    Build the project

---
+++ call_api.py
@@ -0,0 +1,6 @@
+import tejapi as tej
+tej.ApiConfig.api_key = "C2waEjXBx7SBJug2fRgQ8evS2tHrFz"
+data = tej.get("TWN/AAPRTRAN",coid="A",ann_date={"gt":"2023-01-01"},opts={"sort":"tot_prc.desc"},paginate=True)
+
+
+print(data.head())
