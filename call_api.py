bde4481be4793dd77f5b98e839f2395ece57b919
Madison Yang <mymadison0310@gmail.com>
2023-05-06 20:21:35 +0800

    試試看能不能用

--- call_api.py
+++ call_api.py
@@ -3,4 +3,5 @@ tej.ApiConfig.api_key = "C2waEjXBx7SBJug2fRgQ8evS2tHrFz"
 data = tej.get("TWN/AAPRTRAN",coid="A",ann_date={"gt":"2023-01-01"},opts={"sort":"tot_prc.desc"},paginate=True)
 
 
+#hihi
 print(data.head())
